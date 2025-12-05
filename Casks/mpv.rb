241d94618f25d872df8c6796cd5c721c67743379
Myroslav Rys <stonevil@gmail.com>
2025-12-05 03:04:29 -0800

    MPV: fix syntax

--- Casks/mpv.rb
+++ Casks/mpv.rb
@@ -7,14 +7,14 @@ cask "mpv" do
 
   on_arm do
     version "0.40.0"
-    sha256: "3170fb709defebaba33e9755297d70dc3562220541de54fc3d494a8309ef1260",
+    sha256 "3170fb709defebaba33e9755297d70dc3562220541de54fc3d494a8309ef1260"
     url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{arch}-#{version}.tar.gz"
 
    app "mpv-#{arch}-#{version}/mpv.app"
   end
   on_intel do
     version "0.39.0"
-    sha256: "35ec81ad86a97b24956a8d0f4fa1ba2690b44ae7741c920e923620bcd7bd402a",
+    sha256 "35ec81ad86a97b24956a8d0f4fa1ba2690b44ae7741c920e923620bcd7bd402a"
     url "https://laboratory.stolendata.net/~djinn/mpv_osx/mpv-#{version}.tar.gz"
 
    app "mpv-#{version}/mpv.app"
