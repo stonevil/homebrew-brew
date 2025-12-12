ff43ec1707286158bff883fc3740b3f8ae31ce25
Myroslav Rys <stonevil@gmail.com>
2025-12-12 01:25:46 -0800

    LIBREWOLF: up to v146.0.2

---
+++ Casks/librewolf.rb
@@ -0,0 +1,42 @@
+cask "librewolf" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "146.0.2,2"
+  sha256 arm:   "356f4380771a2589156a07f897294b47ca93b09e8d8a21ae2e1648b1208e3033",
+         intel: "a63b3f2e2e1ad497c73d768845b6f8203bb6691ef28e87d6f58ca5cc87a0c664"
+
+  url "https://gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/#{version.tr(",", "-")}/librewolf-#{version.tr(",", "-")}-macos-#{arch}-package.dmg",
+      verified: "gitlab.com/api/v4/projects/44042130/packages/generic/librewolf/"
+  name "LibreWolf"
+  desc "Web browser"
+  homepage "https://librewolf.net/"
+
+  livecheck do
+    url "https://gitlab.com/librewolf-community/browser/bsys6.git"
+    regex(/^v?(\d+(?:[.-]\d+)+)$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.tr("-", ",") }
+    end
+  end
+
+  app "LibreWolf.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/librewolf.wrapper.sh"
+  binary shimscript, target: "librewolf"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/LibreWolf.app/Contents/MacOS/librewolf' "$@"
+    EOS
+  end
+
+  zap trash: [
+    "~/.librewolf",
+    "~/Library/Application Support/LibreWolf",
+    "~/Library/Caches/LibreWolf Community",
+    "~/Library/Caches/LibreWolf",
+    "~/Library/Preferences/io.gitlab.librewolf-community.librewolf.plist",
+    "~/Library/Saved Application State/io.gitlab.librewolf-community.librewolf.savedState",
+  ]
+end
