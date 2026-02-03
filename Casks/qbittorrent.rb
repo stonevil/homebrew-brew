c048f3a1bff9aca3f39e2888568bdeaa78f4dff3
Myroslav Rys <stonevil@gmail.com>
2026-02-03 14:06:47 -0800

    QBITTORRENT: update

--- Casks/qbittorrent.rb
+++ Casks/qbittorrent.rb
@@ -1,20 +1,29 @@
-cask "qbittorrent@lt20" do
-  version "5.0.5"
-  sha256 "f0180356f89590d2de639ba700813fb9ba1d90dde1af5353fc7621a048cccbb2"
+cask "qbittorrent" do
+  on_catalina :or_older do
+    version "4.6.7"
+    sha256 "0b1051af73562fc3f7c0c71abd27c3433ad238fbca0c4612f554db35be3eba6e"
 
-  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}_lt20.dmg",
-      verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
-  name "qBittorrent"
-  desc "Edition of qBitorrent based on libtorrent-rasterbar 2.0.x"
-  homepage "https://www.qbittorrent.org/"
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_big_sur :or_newer do
+    version "5.0.5"
+    sha256 "7285e98b5ef4b9a227247dcd933f1a556bfc1ff10901fc7aedb0c4b7ad082814"
 
-  livecheck do
-    url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
-    regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)[._-]lt20\.dmg}i)
+    livecheck do
+      url "https://sourceforge.net/projects/qbittorrent/rss?path=/qbittorrent-mac"
+      regex(%r{url=.*?/qbittorrent[._-]v?(\d+(?:\.\d+)+)\.dmg}i)
+    end
   end
 
-  conflicts_with cask: "qbittorrent"
-  depends_on macos: ">= :big_sur"
+  url "https://downloads.sourceforge.net/qbittorrent/qbittorrent-mac/qbittorrent-#{version}/qbittorrent-#{version}.dmg",
+      verified: "downloads.sourceforge.net/qbittorrent/qbittorrent-mac/"
+  name "qBittorrent"
+  desc "Peer to peer Bitorrent client"
+  homepage "https://www.qbittorrent.org/"
+
+  conflicts_with cask: "qbittorrent@lt20"
 
   # Renamed for consistency: app name is different in the Finder and in a shell.
   app "qbittorrent.app", target: "qBittorrent.app"
