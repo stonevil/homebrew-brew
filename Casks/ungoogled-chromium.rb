128c527c83d2dbec9433a52dd3d1ab03f62d4a53
Myroslav Rys <stonevil@gmail.com>
2026-02-03 14:07:12 -0800

    CHROMIUM: init commit

---
+++ Casks/ungoogled-chromium.rb
@@ -0,0 +1,41 @@
+cask "ungoogled-chromium" do
+  arch arm: "arm64", intel: "x86_64"
+
+  version "144.0.7559.96-1.1"
+  sha256 arm:   "4cc8236ffeacf8dcf3ba43422cf8d3c3ee28bf0e6eab0498b25e62906952be9a",
+         intel: "c74379c1f66e249267b3c77a443966945ffdd4a6e646bfe5e60752e9d1851663"
+
+  url "https://github.com/ungoogled-software/ungoogled-chromium-macos/releases/download/#{version}/ungoogled-chromium_#{version}_#{arch}-macos.dmg",
+      verified: "github.com/ungoogled-software/ungoogled-chromium-macos/"
+  name "Ungoogled Chromium"
+  desc "Google Chromium, sans integration with Google"
+  homepage "https://ungoogled-software.github.io/"
+
+  livecheck do
+    url :url
+    regex(/^v?(\d+(?:[.-]\d+)+)(?:[._-]#{arch})?(?:[._-]+?(\d+(?:\.\d+)*))?$/i)
+    strategy :github_latest do |json, regex|
+      match = json["tag_name"]&.match(regex)
+      next if match.blank?
+
+      match[1]
+    end
+  end
+
+  conflicts_with cask: "chromium"
+  depends_on macos: ">= :monterey"
+
+  app "Chromium.app"
+
+  postflight do
+    system_command '/usr/bin/xattr',
+      args: ['-r', '-d', 'com.apple.quarantine', "#{appdir}/Chromium.app"]
+  end
+
+  zap trash: [
+    "~/Library/Application Support/Chromium",
+    "~/Library/Caches/Chromium",
+    "~/Library/Preferences/org.chromium.Chromium.plist",
+    "~/Library/Saved Application State/org.chromium.Chromium.savedState",
+  ]
+end
